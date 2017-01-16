ad6d253bdcf9128f49531153f3fd03327c78dd0e
toleberdyyev <fredcolin079@gmail.com>
2017-01-16 14:30:41 +0600

    set an AngularJs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-var app = angular.module("spt", ["ngRoute"]);
+var app = angular.module("spt", []);
+app.controller('sptCtrl', function($scope,$http) {
+  $scope.save = function(){
+  $scope.subjectArray = [];
+  angular.forEach($scope.subject, function(subject){
+    if (!!subject.selected) $scope.subjectArray.push(subject.name);
+  })
+  console.log($scope.subjectArray)
+}
+
+});

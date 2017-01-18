87a93360c8d3859467f9789e45fce26196422292
toleberdyyev <fredcolin079@gmail.com>
2017-01-18 11:27:48 +0600

    made a defualt language json

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 var app = angular.module("spt", []);
 app.controller('sptCtrl', function($scope,$http) {
-  $scope.save = function(){
-  $scope.subjectArray = [];
-  angular.forEach($scope.subject, function(subject){
-    if (!!subject.selected) $scope.subjectArray.push(subject.name);
-  })
-  console.log($scope.subjectArray)
+  $scope.form = {
+    'About':'default',
+    'info':'default',
+    'name':'default',
+    'surname':'default',
+    'email':'default',
+    'iin':'default',
+    'info_SPT':'default',
+    'city':'default',
+    'shool':'default',
+    'subject':'default',
+    'test_language':'default'
+  }
+  $scope.form_rus={
+
+  }
+  // console.log($scope.f)
+  $scope.save = function(name,surname,email,iin,city,school,selectedSub,selectedLan){
+    console.log(name,surname,email,iin,city,school,selectedSub,selectedLan)
 }
 
 });
